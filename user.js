a13adce28c1cac4694cb3fd007e228d0df9e5a67
mayankmandloi <mayankmandloi009@gmail.com>
2017-08-03 14:19:08 +0530

    .

---
+++ user.js
@@ -0,0 +1,47 @@
+var user = {
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "mrs",
+        "first": "rosalyn",
+        "last": "horton"
+      },
+      "location": {
+        "street": "1979 george street",
+        "city": "bath",
+        "state": "west midlands",
+        "postcode": "EZ4V 7GR"
+      },
+      "email": "rosalyn.horton@example.com",
+      "login": {
+        "username": "heavyfrog824",
+        "password": "nirvana",
+        "salt": "tfijr4Md",
+        "md5": "21cc1ec9059f3ea1401acf48b2b649fb",
+        "sha1": "93b21169225792920fa6349a4819c3f4e1b59fae",
+        "sha256": "0f2524465db90af7b78b855f072dbdcf2f491a0257f5665fd8e01c62fc46f3da"
+      },
+      "dob": "1964-04-13 03:21:29",
+      "registered": "2012-04-16 01:17:04",
+      "phone": "0116735 520 6974",
+      "cell": "0716-818-723",
+      "id": {
+        "name": "NINO",
+        "value": "LP 24 08 56 O"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/women/28.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/28.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/28.jpg"
+      },
+      "nat": "GB"
+    }
+  ],
+  "info": {
+    "seed": "47f82662e09f230d",
+    "results": 1,
+    "page": 1,
+    "version": "1.1"
+  }
+}
